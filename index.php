a49e04eca1cb118c707a173c8a0b6fdd9133c9c1
As Syafaa <purpleeisya@gmail.com>
2021-09-30 10:46:48 +0800

    commint

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Kedua</title>
+</head>
+<body>
+    <?php
+    $nama = 'As Syafaa binti Amran';
+    $umur = 22;
+    echo 'Nama kamu $nama<br>';
+    echo "Nama kamu $nama<br>";
+    echo "$nama berumur $umur tahun";
+    ?>
+</body>
+</html>
